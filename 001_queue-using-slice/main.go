b1853483db4b94a0e04ee2d24367b4aae1e5fd31
Vinay Jeurkar <vjeurkar03@gmail.com>
2022-06-16 18:33:23 +0530

    Updated queue using slice implementation

--- 001_queue-using-slice/main.go
+++ 001_queue-using-slice/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"errors"
 	"fmt"
+
+	"github.com/golang-demos/chalk"
 )
 
 type Queue struct {
@@ -25,7 +27,7 @@ func (q *Queue) dequeue() (int, error) {
 		returnItem = item
 		fmt.Println("Retrieved value :", returnItem)
 	} else {
-		fmt.Println("Empty Queue")
+		fmt.Println(chalk.Red(), "No item to retrieve", chalk.Reset())
 		err = errors.New("empty queue")
 	}
 	q.Show()
@@ -33,9 +35,16 @@ func (q *Queue) dequeue() (int, error) {
 }
 
 func (q *Queue) Show() {
-	fmt.Print("Queue : ")
-	for _, item := range q.list {
-		fmt.Print(item, " | ")
+	fmt.Print("  Queue : ")
+	length := len(q.list)
+	if length > 0 {
+		fmt.Print(chalk.YellowLight())
+		for _, item := range q.list {
+			fmt.Print(item, " | ")
+		}
+		fmt.Print(chalk.Reset())
+	} else {
+		fmt.Print(chalk.Red(), "<empty>", chalk.Reset())
 	}
 	fmt.Print("\n")
 }
@@ -49,6 +58,8 @@ func main() {
 	que := getQueue()
 
 	que.enqueue(17)
+	que.dequeue()
+	que.dequeue()
 	que.enqueue(78)
 	que.enqueue(56)
 	que.enqueue(42)
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/golang-demos/go-data-structures
 
 go 1.17
+
+require github.com/golang-demos/chalk v1.1.1
