fd93a3b9fb5d455ed279e705b8cc6c6dfee8a7b8
Vinay Jeurkar <vjeurkar03@gmail.com>
2022-06-16 09:10:34 +0530

    Updated Queue using slice example

---
+++ 001_queue-using-slice/README.md
@@ -0,0 +1,32 @@
+## Queue - Using Slice
+
+This code implements Queue using slice for storing data.
+
+
+### Operations
+- Enqueue
+- Dequeue
+- Print
+
+
+### To run 
+```
+cd 001_queue-using-slice
+go run main.go
+```
+
+### Output
+```
+Adding  17  to queue
+Queue : 17 | 
+Adding  78  to queue
+Queue : 17 | 78 | 
+Adding  56  to queue
+Queue : 17 | 78 | 56 | 
+Adding  42  to queue
+Queue : 17 | 78 | 56 | 42 | 
+Retrieved value : 17
+Queue : 78 | 56 | 42 | 
+Adding  31  to queue
+Queue : 78 | 56 | 42 | 31 | 
+```
--- 001_queue-using-slice/main.go
+++ 001_queue-using-slice/main.go
@@ -10,25 +10,34 @@ type Queue struct {
 }
 
 func (q *Queue) enqueue(item int) {
+	fmt.Println("Adding ", item, " to queue")
 	q.list = append(q.list, item)
+	q.Show()
 }
 
 func (q *Queue) dequeue() (int, error) {
 	length := len(q.list)
+	var err error
+	returnItem := -1
 	if length > 0 {
 		item := q.list[0]
 		q.list = q.list[1:]
-		return item, nil
+		returnItem = item
+		fmt.Println("Retrieved value :", returnItem)
 	} else {
 		fmt.Println("Empty Queue")
-		return -1, errors.New("empty queue")
+		err = errors.New("empty queue")
 	}
+	q.Show()
+	return returnItem, err
 }
 
 func (q *Queue) Show() {
+	fmt.Print("Queue : ")
 	for _, item := range q.list {
 		fmt.Print(item, " | ")
 	}
+	fmt.Print("\n")
 }
 
 func getQueue() *Queue {
@@ -45,6 +54,4 @@ func main() {
 	que.enqueue(42)
 	que.dequeue()
 	que.enqueue(31)
-
-	que.Show()
 }
