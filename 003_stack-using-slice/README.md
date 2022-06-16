9abcf411f1da07f3de2afe9af4792afd5dd90178
Vinay Jeurkar <vjeurkar03@gmail.com>
2022-06-16 21:40:22 +0530

    Updated stack implementation

---
+++ 003_stack-using-slice/README.md
@@ -0,0 +1,33 @@
+## Stack - Using Slice
+
+This code implements Stack using slice for storing data.
+
+
+### Operations
+- Push
+- Pop
+- Print
+
+
+### To run 
+```
+cd 003_stack-using-slice
+go run main.go
+```
+
+### Output
+```
+Pushing  12  to stack
+  Stack :  [12]
+Popped value : 12
+  Stack : <empty>
+Pop() called :   No item to retrieve 
+Pushing  37  to stack
+  Stack :  [37]
+Pushing  82  to stack
+  Stack :  [37 82]
+Popped value : 82
+  Stack :  [37]
+Pushing  29  to stack
+  Stack :  [37 2
+```
--- 003_stack-using-slice/main.go
+++ 003_stack-using-slice/main.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+
+	"github.com/golang-demos/chalk"
+)
 
 type Stack struct {
 	list  []int
@@ -8,19 +13,33 @@ type Stack struct {
 }
 
 func (s *Stack) Push(item int) {
+	fmt.Println("Pushing ", item, " to stack")
 	s.index++
 	s.list = append(s.list, item)
+	s.Show()
 }
 
-func (s *Stack) Pop() int {
+func (s *Stack) Pop() (int, error) {
+	if s.index == -1 {
+		fmt.Println("Pop() called : ", chalk.Red(), "No item to retrieve", chalk.Reset())
+		return -1, errors.New("empty")
+	}
 	item := s.list[s.index]
 	s.list = append(s.list[:s.index], s.list[s.index+1:]...)
 	s.index--
-	return item
+	fmt.Println("Popped value :", item)
+	s.Show()
+	return item, nil
 }
 
 func (s *Stack) Show() {
-	fmt.Println(s.list)
+	fmt.Print("  Stack : ")
+	if len(s.list) > 0 {
+		fmt.Print(chalk.YellowLight(), s.list, chalk.Reset())
+	} else {
+		fmt.Print(chalk.Red(), "<empty>", chalk.Reset())
+	}
+	fmt.Print("\n")
 }
 
 func GetStack() *Stack {
@@ -33,10 +52,11 @@ func main() {
 	s1 := GetStack()
 
 	s1.Push(12)
+	s1.Pop()
+	s1.Pop()
 	s1.Push(37)
+	s1.Push(82)
 	s1.Pop()
 	s1.Push(29)
-
-	s1.Show()
 
 }
